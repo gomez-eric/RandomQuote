5b154a3bdb5e3a92abca340839d8bc13eef11b5a
Eric <gomez123098@yahoo.com>
2019-02-22 03:53:57 -0600

    API URL change, cleaned code, fixed twitter url.

--- randomQuote.js
+++ randomQuote.js
@@ -1,99 +1,48 @@
-//https://talaikis.com/api/quotes/random/
-// daquote ["Quote Will Go Here."]
-// getQuote [BUTTON]
+let color_object = { "mainColor":"200" , "mainHue":"20", "secondHue":"20", "opacity":"1" };
+let twitter_url = "";
+//let apiurl = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+let apiurl = "http://quotes.stormconsultancy.co.uk/random.json";
 
-//Get color JavaScript Object //{"200" "10%" "10%" "1"};
-var colorJSON = '{ "mainColor":"200" , "mainHue":"20", "secondHue":"20", "opacity":"1" }';
-var objDaColor = JSON.parse(colorJSON);
-// For tiwtter URL
-var entireUrl = "";
-// All jQuery Functionss
+var func_getQuote = function(){
+  makingPal(); // Randomly generate colors
+  // Set the root colors
+  $(':root').css({'--daColor': get_hsla_color(color_object.mainHue, color_object.opacity)})
+  $(':root').css({'--daBtnColor': get_hsla_color(color_object.secondHue, color_object.opacity-.40)})
+  $("#tweeterButton").show();
 
-var thisWORK = function(){
+  $.ajax({
+    url: apiurl,
+    dataType: 'json',
+    success: function(jsonp){
+      var string = (JSON.stringify(jsonp));
+      var obj = JSON.parse(string);
+      console.log(obj);
+      var icon = "<span class=\"glyphicon glyphicon-comment\" aria-hidden=\"true\"></span>"
+      $(".daQuote").html(obj.quote);
+      $(".daAuthor").html(icon + " " + obj.author);
+      $(".daCat").html("ID:" + obj.id);
 
-  // This test is to retrieve data from the API
-  $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
-    // Get string into a variable
-    var string = (JSON.stringify(json));
-    // Convert string into object
-    var obj = JSON.parse(string);
-    // For icon
-    var icon = "<span class=\"glyphicon glyphicon-comment\" aria-hidden=\"true\"></span>"
-    $(".daQuote").html(obj.quote);
-    $(".daAuthor").html(icon + " " + obj.author);
+      // Make URL for tweet
+      var theTweetUrl="https://twitter.com/intent/tweet?text=";
+      var quoteArray = obj.quote.split(" ");
+      var authorArray = obj.author.split(" ");
+      twitter_url = theTweetUrl;
+      twitter_url += quoteArray.join("+");
+      twitter_url += "++-";
+      twitter_url += authorArray.join("+");
+      var tinyurl = "https://goo.gl/76YWfG";
+      twitter_url + "++" + tinyurl;
 
-    var capCat = obj.cat.toUpperCase();
-    $(".daCat").html(capCat);
-
-    // For Main Color
-    function randomColor() {
-      min = Math.ceil(10);
-      max = Math.floor(360);
-      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
     }
-    // For Hue Value
-    function randomHueValue() {
-      min = Math.ceil(40);
-      max = Math.floor(45);
-      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-    // Make color for background
-    function getColor() {
-      var newColor = "";
-      //hsla(200, 20%, 20%, 1)
-      objDaColor.mainColor = randomColor();
-      objDaColor.mainHue = randomHueValue();
-      objDaColor.secondHue = objDaColor.mainHue-40 + "%";
-      objDaColor.mainHue = objDaColor.mainHue + "%";
-      newColor += "hsla(";
-      newColor += objDaColor.mainColor+", ";
-      newColor += objDaColor.mainHue+", ";
-      newColor += objDaColor.mainHue+", ";
-      newColor += objDaColor.opacity+")";
-      console.log(newColor);
-      return newColor;
-    }
-    // Make color for button
-    function getBtnColor() {
-      var newBtnColor = "";
-      newBtnColor += "hsla(";
-      newBtnColor += objDaColor.mainColor+", ";
-      newBtnColor += objDaColor.secondHue+", ";
-      newBtnColor += objDaColor.secondHue+", ";
-      newBtnColor += objDaColor.opacity-.40 +")";
-      console.log(newBtnColor);
-      return newBtnColor;
-    }
-    // Make URL for tweet
-    var theTweetUrl="https://twitter.com/intent/tweet?text=";
-    var quoteArray = obj.quote.split(" ");
-    var authorArray = obj.author.split(" ");
-    entireUrl = theTweetUrl;
-    entireUrl += quoteArray.join("+");
-    entireUrl += "++-";
-    entireUrl += authorArray.join("+");
-    var tinyurl = "https://goo.gl/76YWfG";
-    entireUrl += "++" + tinyurl;
-    console.log(entireUrl);
-    // Set the root colors
-    $(':root').css({'--daColor': getColor()})
-    $(':root').css({'--daBtnColor': getBtnColor()})
-    $("#tweeterButton").show();
-      });
+  });
+
+
 }
 
+
 $(document).ready(function() {
-  /*$("body").addClass("animated fadeIn").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-    $(this).removeClass("animated fadeIn");
-  });*/
-
   $("#tweeterButton").hide();
-
-  //$("#getQuote").on("click", thisWORK);
-
-  $("#getQuote").on("mousedown", thisWORK);
-
-
+  $("#getQuote").on("mousedown", func_getQuote);
   $("#getQuote").mousedown();
   $(".jumbotron").addClass("animated bounceIn");
 
@@ -104,10 +53,39 @@ $(document).ready(function() {
   });
 
   $('#tweeterButton').click(function() {
-    //window.location=entireUrl;
-    window.open(entireUrl, '_blank');
+    window.open(twitter_url, '_blank');
   });
 
-
-
 });
+
+
+// For Main Color
+function randomColor() {
+  min = Math.ceil(10);
+  max = Math.floor(360);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+// For Hue Value
+function randomHueValue() {
+  min = Math.ceil(40);
+  max = Math.floor(45);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function makingPal(){
+  color_object.mainColor = randomColor();
+  color_object.mainHue = randomHueValue();
+  color_object.secondHue = color_object.mainHue-40 + "%";
+  color_object.mainHue = color_object.mainHue + "%";
+}
+
+// Make color for background
+function get_hsla_color(x,y) {
+  var color_return = "";
+  //hsla(200, 20%, 20%, 1)
+  color_return += "hsla(";
+  color_return += color_object.mainColor+", ";
+  color_return += x + ", " + x + ", ";
+  color_return += y + ")";
+  return color_return;
+}
